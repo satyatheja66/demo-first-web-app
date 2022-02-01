85ab469ffe5ae56a95dd382ae2052260172ba9a1
satyatheja66 <satyatheja66@gmail.com>
2022-01-31 22:48:39 -0500

    data

---
+++ src/main/java/com/example/demofirstwebapp/DemoFirstWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demofirstwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFirstWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFirstWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demofirstwebapp/DemoFirstWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demofirstwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFirstWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
